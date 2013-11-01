b0b6b817af3ce1be70eaa797aa3731ba7eab4114
Troy Daniel <troy.daniel@mosaic.com>
2013-11-01 01:15:21 -0400

    Ran it through JSLint

--- js/product.js
+++ js/product.js
@@ -1,62 +1,59 @@
 function Product(options) {
 
-  // Initialize using options
-  this.numOfPeople = options.numOfPeople;
-  this.basePrice = options.basePrice;
-  this.material = options.material;
+    // Configurable markup costs in percentages
+    var markup = {
+        flat: 0.05,
+        peopleFactor: 0.012,
+        material: {
+            food: 0.13,
+            drugs: 0.075,
+            electronics: 0.02
+        }
+    };
 
-  // PUBLIC METHODS
-  // --
+    // Initialize using options
+    this.numOfPeople = options.numOfPeople;
+    this.basePrice = options.basePrice;
+    this.material = options.material;
 
-  // Calculate the total cost to package the goods
-  Product.prototype.totalCost = function() {
-    var totalCost = this.flatWithBase() + this.costForPeople() + this.costForMaterials();
-    return parseFloat(totalCost.toFixed(2));
-  };
-
-  // Calculate the minimum basePrice+flatRate
-  Product.prototype.flatWithBase = function() {
-    var flatWithBase = this.basePrice * (1 + markup.flat);
-    return parseFloat(flatWithBase.toFixed(2));
-  };
-
-  // Calculate the line item cost for 'people'
-  Product.prototype.costForPeople = function() {
-    var costForPeople = this.flatWithBase() * peopleFactor(this);
-    return parseFloat(costForPeople.toFixed(2));
-  };
-
-  // Calculate the line item cost for 'materials'
-  Product.prototype.costForMaterials = function() {
-    var costForMaterials = this.flatWithBase() * markupForMaterial(this.material);
-    return parseFloat(costForMaterials.toFixed(2));
-  };
-
-  // PRIVATE METHODS
-  // --
-
-  // Calc the markup based on the # of ppl working
-  function peopleFactor(that) {
-    return markup.peopleFactor * that.numOfPeople;
-  }
-
-  // Return the markup for food, drugs, electronics or zero
-  function markupForMaterial(m) {
-    var materialFactor = markup.material[m];
-    if (typeof materialFactor === "undefined")
-      return 0;
-    else
-      return markup.material[m];
-  }
-
-  // Configurable markup costs in percentages
-  var markup = {
-    flat: 0.05,
-    peopleFactor: 0.012,
-    material: {
-      food: 0.13,
-      drugs: 0.075,
-      electronics: 0.02
+    // PRIVATE METHOD: Calc the markup based on the # of ppl working
+    function peopleFactor(that) {
+        return markup.peopleFactor * that.numOfPeople;
     }
-  };
+
+    // PRIVATE METHOD: Return the markup for food, drugs, electronics or zero
+    function markupForMaterial(m) {
+        var materialFactor = markup.material[m];
+        if (typeof materialFactor === "undefined") {
+            return 0;
+        }
+        return markup.material[m];
+    }
+
+    // PUBLIC METHODS
+    // --
+    
+    // Calculate the total cost to package the goods
+    Product.prototype.totalCost = function() {
+        var totalCost = this.flatWithBase() + this.costForPeople() + this.costForMaterials();
+        return parseFloat(totalCost.toFixed(2));
+    };
+
+    // Calculate the minimum basePrice+flatRate
+    Product.prototype.flatWithBase = function() {
+        var flatWithBase = this.basePrice * (1 + markup.flat);
+        return parseFloat(flatWithBase.toFixed(2));
+    };
+
+    // Calculate the line item cost for 'people'
+    Product.prototype.costForPeople = function() {
+        var costForPeople = this.flatWithBase() * peopleFactor(this);
+        return parseFloat(costForPeople.toFixed(2));
+    };
+
+    // Calculate the line item cost for 'materials'
+    Product.prototype.costForMaterials = function() {
+        var costForMaterials = this.flatWithBase() * markupForMaterial(this.material);
+        return parseFloat(costForMaterials.toFixed(2));
+    };
 }
